b5cdf160e3e4624a18ac10769333f24b6d97014a
Mikhail Natalevich <natalevichmv@gmail.com>
2022-12-10 02:54:50 +0300

    Fetch www.google.com

--- poco_request.cpp
+++ poco_request.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 
 #include <Poco/Net/HTTPClientSession.h>
 #include <Poco/Net/HTTPRequest.h>
@@ -8,24 +9,32 @@
 #include <Poco/StreamCopier.h>
 
 int main() {
-    Poco::URI uri("http://jsonplaceholder.typicode.com/todos/1");
+    // create session
+    Poco::URI uri("http://www.google.com");
     Poco::Net::HTTPClientSession session(uri.getHost(), uri.getPort());
 
+    // get path to resourse
     std::string path(uri.getPathAndQuery());
     if (path.empty()) {
         path = "/";
     }
+
+    // create request
     Poco::Net::HTTPRequest request(
         Poco::Net::HTTPRequest::HTTP_GET,
         path,
         Poco::Net::HTTPMessage::HTTP_1_1);
     session.sendRequest(request);
 
+    // receive response
     Poco::Net::HTTPResponse response;
     std::istream& in = session.receiveResponse(response);
-    Poco::StreamCopier::copyStream(in, std::cout);
 
-    std::cout << std::endl;
+    // write response to file
+    std::ofstream fout("page.html");
+    Poco::StreamCopier::copyStream(in, fout);
+
+    // report code
     std::cout << "Response code: " << response.getStatus() 
               << " " << response.getReason() << std::endl;
     return 0;
